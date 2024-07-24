0a08586c9c13be5fcaac0c905b028fdf3373f13a
Prashant Shinde <prashantshinde2573@gmail.com>
2024-07-24 21:02:03 +0530

    added all files

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+const App=()=>{
+  const [color,setcolor]=useState("white");
+
+  return(
+    <div className="bg-red-500 w-full h-screen duration-200 flex flex "style={{backgroundColor:color}}>
+      <div className="bg-white w-11/12 m-auto p-3 flex justify-around" >
+      <button onClick={()=>{setcolor("red")}} className="w-24 h-8 text-white font-bold hover:border-4 border-black" style={{backgroundColor:"red"}}>Red</button>
+      <button onClick={()=>{setcolor("yellow")}} className="w-24 h-8 text-white font-bold hover:border-4 border-black" style={{backgroundColor:"yellow"}}>Yellow</button>
+      <button onClick={()=>{setcolor("Pink")}} className="w-24 h-8 text-white font-bold hover:border-4 border-black " style={{backgroundColor:"pink"}}>Pink</button>
+      <button onClick={()=>{setcolor("Orange")}} className="w-24 h-8 text-white font-bold hover:border-4 border-black" style={{backgroundColor:"orange"}}>Orange</button>
+      <button onClick={()=>{setcolor("Purple")}} className="w-24 h-8 text-white font-bold hover:border-4 border-black" style={{backgroundColor:"purple"}}>Purple</button>
+      <button onClick={()=>{setcolor("Black")}} className="w-24 h-8 text-white font-bold hover:border-4 border-black" style={{backgroundColor:"black"}}>Black</button>
+      </div>
+      <h2 className="m-auto">BACKGROUND CHANGER WITH HELP OF REACT !</h2>
+
+    </div>
+  )
+
+}
+
+export default App;
